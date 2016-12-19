ea30fe9ca2f04e03de6516934afcf24c34cd0820
hjoo5 <gusebdj@hanmail.net>
2016-12-19 20:11:13 +0900

    baekjoon
    1920

---
+++ baekjoon/1920.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int n,i;
+	int a[100001]={};
+	scanf("%d", &n);
+	for(i=0;i<n;i++){
+		scanf("%d", &a[i]);
+	}
+	scanf("%d", &m);
+	for(i=0;i<m;i++){
+		scanf("%d", &)
+	}
+	return 0;
+}
